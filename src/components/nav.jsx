46997f8f99be40de702ea189d062c591c7c0ae64
Charan Rai <122370376+CharanRai777@users.noreply.github.com>
2024-02-25 13:40:17 +0530

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,10 +8,10 @@ const Nav = () => {
           <span className="italic text-3xl text-white ml-10">fundrise</span>
         </div>
         <ul className="flex justify-evenly space-x-12">
-          <li><a href="#" className="text-white hover:text-blue-400">Home</a></li>
-          <li><a href="#" className="text-white hover:text-blue-400">Location</a></li>
-          <li><a href="#" className="text-white hover:text-blue-400">About</a></li>
-          <li><a href="#" className="text-white hover:text-blue-400">Contact</a></li>
+          <li><a href="#" className="text-white hover:text-blue-900">Home</a></li>
+          <li><a href="#" className="text-white hover:text-blue-900">Location</a></li>
+          <li><a href="#" className="text-white hover:text-blue-900">About</a></li>
+          <li><a href="#" className="text-white hover:text-blue-900">Contact</a></li>
         </ul>
         <button className="hidden sm:block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-10 rounded">
           Login
